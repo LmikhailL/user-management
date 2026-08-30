28793e632c033e4ef768fc61577bc52fbe567597
Mike <lipilovmsh@gmail.com>
2026-08-30 22:37:14 +0300

    removed redundant public keyword from main method

--- src/test/java/org/mike/usermanagement/TestUserManagementApplication.java
+++ src/test/java/org/mike/usermanagement/TestUserManagementApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 
 public class TestUserManagementApplication {
 
-    public static void main(String[] args) {
+    static void main(String[] args) {
         SpringApplication.from(UserManagementApplication::main)
                 .with(TestcontainersConfiguration.class)
                 .run(args);
